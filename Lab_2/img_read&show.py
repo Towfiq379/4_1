bfb21b2e9bcb6e61ff923dd2d2c14e3954e564f2
Towfiq379 <36331420+Towfiq379@users.noreply.github.com>
2020-03-04 03:48:06 +0600

    Add files via upload

---
+++ Lab_2/img_read&show.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+img = plt.imread('C:\\Users\\asus\\AppData\\Local\\Programs\\Python\\Python38\\img\\onion.png')
+plt.imshow(img);
+plt.axis('off')
+plt.title('ONIONS')
+plt.show()
